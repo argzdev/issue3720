45259e38fcaf4afaf7ed74d38d3baa1234de4843
argzdev <alvinrustan@google.com>
2022-05-17 18:50:05 +0800

    Initial commit

---
+++ app/src/main/java/com/argz/issue3720/MainActivity.java
@@ -0,0 +1,14 @@
+package com.argz.issue3720;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
